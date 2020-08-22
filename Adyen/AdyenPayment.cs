7fe633ec0e3b54389556551b54607f89426e1b92
adamstrain_gcloud <adamstrain.gcloud@gmail.com>
2020-08-22 12:53:41 +0000

    Add project files.

---
+++ Adyen/AdyenPayment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Adyen;
+using Adyen.Model.Checkout;
+using Adyen.Service;
+using mod = Adyen.Model;
+
+namespace Barfly.Payments.Adyen
+{
+    public class AdyenPayment
+    {
+        public string getPaymentMethods(int paymentValue)
+        {
+            var client = new Client("AQE7hmfuXNWTK0Qc+iSRlmU1lPCJSYJDPpxMTHJX1H2Ym3Rdl8RzA+p1AmA4WftafpYUaU5ZdfdhAJd3XEEQwV1bDb7kfNy1WIxIIkxgBw==-vWBMpptmnKKvlSHqmG+ne84CFkkG8tB+FwFLO2puXeU=-+Z6f4p>A{QB;4U9+", mod.Enum.Environment.Test);
+            var checkout = new Checkout(client);
+            var amount = new mod.Checkout.Amount("GBP", paymentValue);
+            var paymentMethodsRequest = new mod.Checkout.PaymentMethodsRequest(merchantAccount: "AdamStrainSoftwareServicesLtd797ECOM")
+            {
+                CountryCode = "GB",
+                ShopperLocale = "en_US",
+                Amount = amount,
+                Channel = PaymentMethodsRequest.ChannelEnum.Web
+            };
+
+            var paymentMethodsResponse = checkout.PaymentMethods(paymentMethodsRequest).ToJson();
+            return paymentMethodsResponse;
+
+        }
+    }
+}
---
+++ Models/PaymentRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barfly.Payments.Models
+{
+    public class PaymentRequest
+    {
+        public string orderToken { get; set; }
+        public int? orderValue { get; set; }
+        public string name { get; set; }
+        public string orderDescription { get; set; }
+        public string customerOrderCode { get; set; }
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string city { get; set; }
+        public string countryCode { get; set; }
+        public string postalCode { get; set; }
+    }
+}
