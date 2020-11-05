54549e757eac9d3862129f65be7950c02c1a91d4
adamstrain.cibo <adamstrain.gcloud@gmail.com>
2020-11-05 14:05:14 +0000

    Updated Adyen

--- Adyen/AdyenPayment.cs
+++ Adyen/AdyenPayment.cs
@@ -30,18 +30,17 @@ namespace Barfly.Payments.Adyen
             return paymentMethodsResponse;
         }
 
-        public PaymentsResponse makePayment(mod.Checkout.DefaultPaymentMethodDetails req)
+        public PaymentsResponse makePayment(mod.Checkout.DefaultPaymentMethodDetails req, int value, string reference)
         {
             try
             {
                 var client = new Client("AQE7hmfuXNWTK0Qc+iSRlmU1lPCJSYJDPpxMTHJX1H2Ym3Rdl8RzA+p1AmA4WftafpYUaU5ZdfdhAJd3XEEQwV1bDb7kfNy1WIxIIkxgBw==-vWBMpptmnKKvlSHqmG+ne84CFkkG8tB+FwFLO2puXeU=-+Z6f4p>A{QB;4U9+", mod.Enum.Environment.Test);
                 var checkout = new Checkout(client);
-
-                var amount = new mod.Checkout.Amount("GBP", 1000);
+                var amount = new mod.Checkout.Amount("GBP", value);
                 var details = req;
                 var paymentRequest = new mod.Checkout.PaymentRequest
                 {
-                    Reference = "123456789",
+                    Reference = reference,
                     Amount = amount,
                     ReturnUrl = @"https://example.com/checkout?shopperOrder",
                     MerchantAccount = "AdamStrainSoftwareServicesLtd797ECOM",
--- Models/Adyen/PaymentRequest.cs
+++ Models/Adyen/PaymentRequest.cs
@@ -10,6 +10,7 @@ namespace Barfly.Payments.Models.Adyen
     public class AdyenPaymentRequest
     {
         public int organisationId { get; set; }
+        public int value { get; set; }
         public mod.Checkout.DefaultPaymentMethodDetails paymentMethod {get;set;}
     }
 }
